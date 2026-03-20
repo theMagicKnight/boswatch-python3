0e2bfe3b056fdb95a11c8d18f16bc0972105940c
theMagicKnight <96948811+theMagicKnight@users.noreply.github.com>
2026-03-20 08:49:24 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,7 +29,9 @@ echo "   / __  / / / /\__ \| | /| / / __  / __/ ___/ __ \ "
 echo "  / /_/ / /_/ /___/ /| |/ |/ / /_/ / /_/ /__/ / / / "
 echo " /_____/\____//____/ |__/|__/\__,_/\__/\___/_/ /_/  "
 echo "            German BOS Information Script           "
-echo "                 by Bastian Schroll                 "
+echo "            by Bastian Schroll and chatGPT          "
+echo ""
+echo "					Version Python3        			  "
 echo ""
 
 # Make sure only root can run our script
@@ -119,9 +121,9 @@ echo "-> download BOSWatch..................."
 cd $boswatchpath/
 
 case $branch in
-  "dev") git clone -b develop https://github.com/Schrolli91/BOSWatch . >> $boswatch_install_path/setup_log.txt 2>&1 && \
+  "dev") git clone -b develop https://github.com/theMagicKnight/boswatch-python3 . >> $boswatch_install_path/setup_log.txt 2>&1 && \
     exitcodefunction $? git-clone BOSWatch-develop ;;
-  *) git clone -b master https://github.com/Schrolli91/BOSWatch . >> $boswatch_install_path/setup_log.txt 2>&1 && \
+  *) git clone -b master https://github.com/theMagicKnight/boswatch-python3 . >> $boswatch_install_path/setup_log.txt 2>&1 && \
     exitcodefunction $? git-clone BOSWatch ;;
 esac
 
@@ -131,7 +133,7 @@ echo "[ 4/9] [####-----]"
 tput cup 15 5
 echo "-> download rtl_fm......................"
 cd $boswatch_install_path
-git clone https://github.com/Schrolli91/rtl-sdr.git >> $boswatch_install_path/setup_log.txt 2>&1
+git clone https://github.com/osmocom/rtl-sdr.git >> $boswatch_install_path/setup_log.txt 2>&1
 exitcodefunction $? git-clone rtl-sdr
 cd rtl-sdr/
 
@@ -160,7 +162,7 @@ echo "[ 6/9] [######---]"
 tput cup 15 5
 echo "-> download multimon-ng................"
 cd $boswatch_install_path
-git clone https://github.com/Schrolli91/multimon-ng.git multimonNG >> $boswatch_install_path/setup_log.txt 2>&1
+git clone https://github.com/EliasOenal/multimon-ng multimonNG >> $boswatch_install_path/setup_log.txt 2>&1
 exitcodefunction $? git-clone multimonNG
 
 cd $boswatch_install_path/multimonNG/
